ca629a864961dd65119d159aa864809d0325bdda
Axa Mei Liauw <aliauw@princeton.edu>
2016-02-09 12:06:52 -0500

    Add CAS module

---
+++ modules/cas/src/Exception/CasLoginException.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * @file
+ * Contains \Drupal\cas\Exception\CasLoginException.
+ */
+
+namespace Drupal\cas\Exception;
+
+/**
+ * Class CasLoginException.
+ */
+class CasLoginException extends \Exception {
+}
